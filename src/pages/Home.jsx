7ef8654d1ca18176d0aefa044d7a87cdc97afd90
nitinmasih <nitin.masih02@gmail.com>
2024-07-26 02:07:21 +0530

    change in experience section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import About from "../components/About"
+import React, { useState, useEffect } from "react";
+import About from "../components/About";
 import Info from "../components/Info";
 import OurWorkAnimation from "../components/OurWorkAnimation";
 import Experience from "../components/Experience";
@@ -8,12 +8,25 @@ import ContactInfo from "../components/ContactInfo";
 import TrustedCollaborators from "../components/TrustedCollaborators";
 
 const Home = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
       <About />
       <Info />
       <OurWorkAnimation />
-      <Experience />
+      {(windowWidth < 450 || windowWidth > 1200) && <Experience />}
       <WorkingBenefits />
       <TrustedCollaborators />
       <ContactInfo />
